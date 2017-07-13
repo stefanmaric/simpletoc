ce4f69a5784ab703f635d1cd8aeec4ba4a570e3a
Stefan Maric <me@stefanmaric.com>
2017-07-13 18:32:00 -0500

    Add tests for mdToc function

---
+++ test/md-toc.spec.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const test = require('tap').test
+const mdToc = require('../src/lib/md-toc.js')
+
+test('mdToc should, by default, replace lines that contain no more than "TOC"', t => {
+  const src = 'TOC'
+  const expected = ''
+  const result = mdToc(src)
+
+  t.strictSame(result, expected)
+  t.end()
+})
+
+test('mdToc should replace text that matches the target regex, if given', t => {
+  const src = 'Lorem ipsum dolor sit amet'
+  const expected = 'Lorem ipsum  sit amet'
+  const result = mdToc(src, { target: /dolor/ })
+
+  t.strictSame(result, expected)
+  t.end()
+})
+
+test('mdToc should generate a table of contents and replace the TOC line with it', t => {
+  const src = `
+
+TOC
+
+# First
+
+Lorem ipsum dolor sit amet
+
+## Second
+
+Lorem ipsum dolor sit amet
+
+### Third
+
+Lorem ipsum dolor sit amet
+`
+  const expected = `
+
+1. [First](#first)
+    1. [Second](#second)
+        1. [Third](#third)
+
+# First
+
+Lorem ipsum dolor sit amet
+
+## Second
+
+Lorem ipsum dolor sit amet
+
+### Third
+
+Lorem ipsum dolor sit amet
+`
+
+  const result = mdToc(src)
+
+  t.strictSame(result, expected)
+  t.end()
+})
