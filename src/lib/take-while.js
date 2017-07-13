1c95a76e47e17365eb3eb78048aa2331e79e7b62
Stefan Maric <me@stefanmaric.com>
2017-07-13 18:14:35 -0500

    Add takeWhile function
    See: https://lodash.com/docs/4.17.4#takeWhile

---
+++ src/lib/take-while.js
@@ -0,0 +1,30 @@
+'use strict'
+
+module.exports = takeWhile
+
+/**
+ * Creates a slice of `arr` with elements taken from the beginning. Elements are
+ * taken until `predicate` returns falsey. The predicate is invoked with three
+ * arguments: (value, index, arr).
+ *
+ * @static
+ * @private
+ * @param {Function} fn The function invoked per iteration.
+ * @param {Array} arr The array to query.
+ * @returns {Array} Returns the slice of `arr`.
+ * @example
+ *
+ * const users = [
+ *   { user: 'john', active: false },
+ *   { user: 'mike', active: false },
+ *   { user: 'berto', active: true }
+ * ]
+ *
+ * takeWhile(u => !u.active, users)
+ * // => [{ user: 'john', active: false }, { user: 'mike', active: false }]
+ */
+function takeWhile (fn, arr, i = 0) {
+  return i < arr.length && fn(arr[i], i, arr)
+    ? takeWhile(fn, arr, ++i)
+    : arr.slice(0, i)
+}
